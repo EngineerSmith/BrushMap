766977f7606375a06e970b3cf82118d7bd3e3242
EngineerSmith <pasmith.bus@gmail.com>
2021-03-17 23:46:16 +0000

    Added numeric input element for animation hang time for the animation tab

--- scene/ui/tilesetEditor/tabAnimation.lua
+++ scene/ui/tilesetEditor/tabAnimation.lua
@@ -4,6 +4,8 @@ local tabWindow = require("ui.tabWindow")
 local anchor = require("ui.base.anchor")
 local imageAnimation = require("ui.imageAnimation")
 local buttonFrameSelect = require("ui.buttonFrameSelect")
+local text = require("ui.text")
+local numericInput = require("ui.numericInput")
 
 return function(font, controller)
 local tabAnimation = tabWindow.new("Animation", font)
@@ -20,6 +22,17 @@ tabAnimation.createUI = function(self)
     local anchor = anchor.new("NorthWest", 10, 10+height, w,w/3, 20,0)
     self.frameSelect = buttonFrameSelect.new(anchor, global.assets["font.robotoReg25"], global.assets["icon.plus"])
     self:addChild(self.frameSelect)
+    
+    local x,y,w,h = self.frameSelect.anchor:getRect()
+    local height = y + h
+    
+    local anchor = anchor.new("NorthWest", 10, 10+height, -1,20, 20,0)
+    local titleTime = text.new(anchor, "Time (Seconds)", font)
+    local anchor = anchor.new("NorthWest", 10, 40+height, -1,40, 20,0)
+    self.time = numericInput.new(anchor, 0.05, 3000, 0.2, font, 0.05)
+    self:addChild(titleTime)
+    self:addChild(self.time)
+    
 end
 
 return tabAnimation
