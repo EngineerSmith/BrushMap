362b188d6a40a22b597c62430277fabe97bef55f
EngineerSmith <pasmith.bus@gmail.com>
2021-03-17 23:50:33 +0000

    Added 3 button elements to animation tab. Delete Frame, Create Tile, Delete Title

--- scene/ui/tilesetEditor/tabAnimation.lua
+++ scene/ui/tilesetEditor/tabAnimation.lua
@@ -6,6 +6,7 @@ local imageAnimation = require("ui.imageAnimation")
 local buttonFrameSelect = require("ui.buttonFrameSelect")
 local text = require("ui.text")
 local numericInput = require("ui.numericInput")
+local button = require("ui.button")
 
 return function(font, controller)
 local tabAnimation = tabWindow.new("Animation", font)
@@ -33,6 +34,20 @@ tabAnimation.createUI = function(self)
     self:addChild(titleTime)
     self:addChild(self.time)
     
+    local anchor = anchor.new("NorthWest", 10,100+height, -1,40, 20,0)
+    local deleteFrame = button.new(anchor)
+    deleteFrame:setText("Delete Frame", nil, font)
+    self:addChild(deleteFrame)
+    
+    local anchor = anchor.new("NorthWest", 10,150+height, -1,40, 20,0)
+    local create = button.new(anchor)
+    create:setText("Create Tile", nil, font)
+    self:addChild(create)
+    
+    local anchor = anchor.new("NorthWest", 10,200+height, -1,40, 20,0)
+    local delete = button.new(anchor)
+    delete:setText("Delete Title", nil, font)
+    self:addChild(delete)
 end
 
 return tabAnimation
